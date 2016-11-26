39e95c67bae9f690712dad7a55fe82627638dc57
Adrian Yem <adrianyem91@outlook.com>
2016-11-26 16:44:16 +0800

    added devise and facebook omniauth without test

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'devise'
 gem 'bootstrap-sass', '~>3.3.6'
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_scope :user do
+    get "/users/sign_out", :to => "devise/sessions#destroy" 
+  end
   root to: 'home#index'
   get '/home', to: 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
 end
